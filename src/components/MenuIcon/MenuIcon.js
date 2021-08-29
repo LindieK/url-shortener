669ecc065eb51f5cfd52784db07ddf4a84e8740f
Linda Okorie <lindaokorie27@gmail.com>
2021-08-29 01:29:40 +0100

    linked to MenuIcon stylesheet

--- src/components/MenuIcon/MenuIcon.js
+++ src/components/MenuIcon/MenuIcon.js
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import "./MenuIcon.scss";
+
 const MenuIcon = ({ handleClick }) => {
   return (
     <div data-testid="menu-icon" id="menu-icon" onClick={handleClick}>
