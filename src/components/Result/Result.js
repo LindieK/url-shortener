679ea7dea84d38dc8646d4513d8a34ce3217219e
Linda Okorie <lindaokorie27@gmail.com>
2021-09-25 02:22:10 +0100

    assed button functionality

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -4,12 +4,16 @@ import Button from "../Button/Button";
 import "./Result.scss";
 
 const Result = (props) => {
+  const handleCopyButtonClick = () => {
+    navigator.clipboard.writeText(props.shortenedLink);
+  };
+
   return (
     <div data-testid="result" className="result">
       <span className="fullLink">{props.fullLink}</span>
       <div className="shortenedDetails">
         <span className="shortenedLink">{props.shortenedLink}</span>
-        <Button value="Copy" />
+        <Button value="Copy" handleClick={handleCopyButtonClick} />
       </div>
     </div>
   );
