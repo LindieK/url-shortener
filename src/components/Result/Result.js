b7433e1af65889846f7d5da4c1a17bfd87379981
Linda Okorie <lindaokorie27@gmail.com>
2021-09-30 17:23:17 +0100

    added copy state and actions

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Button from "../Button/Button";
 import "./Result.scss";
 
 const Result = (props) => {
+  const [buttonText, setButtonText] = useState("Copy");
+  const [buttonClicked, setButtonClicked] = useState(false);
   const handleCopyButtonClick = () => {
+    setButtonText("Copied");
+    setButtonClicked(true);
     navigator.clipboard.writeText(props.shortenedLink);
   };
 
@@ -13,7 +17,11 @@ const Result = (props) => {
       <span className="fullLink">{props.fullLink}</span>
       <div className="shortenedDetails">
         <span className="shortenedLink">{props.shortenedLink}</span>
-        <Button value="Copy" handleClick={handleCopyButtonClick} />
+        <Button
+          value={buttonText}
+          classNames={buttonClicked ? "copied" : null}
+          handleClick={handleCopyButtonClick}
+        />
       </div>
     </div>
   );
