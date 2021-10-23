027d7befd0c8e04f39cea94862ca13d8abd4df3a
Linda Okorie <lindaokorie27@gmail.com>
2021-10-23 17:00:05 +0100

    doc: corrected spelling of "response" in function

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 export const getShortenedURL = (inputValue) => {
   if(inputValue){
     const url = `https://api.shrtco.de/v2/shorten?url=${inputValue}`;
-    return axios.get(url).then(respose => response)
+    return axios.get(url).then(response => response)
   }
 }
