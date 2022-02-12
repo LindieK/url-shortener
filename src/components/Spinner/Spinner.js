20d81faa7d5762bd1d7f3dccbe3510e6754229c2
Linda Okorie <lindaokorie27@gmail.com>
2022-02-12 11:19:51 +0100

    feat: created a spinner component

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./Spinner.scss";
+
+const Spinner = () => {
+  return (
+    <div id="middle">
+      <div id="inner"></div>
+    </div>
+  );
+};
+export default Spinner;
